3a93d3064f01c6062e9aedb6e5cf0cabea17d3d2
@hexlet/cli <support@hexlet.io>
2023-03-29 01:32:36 +0500

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -4,12 +4,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-// BEGIN
 public class App {
-    public static List<String> buildApartmentsList(List<Home> homes, int n) {
 
-        homes.sort((h1, h2) -> Double.compare(h1.getArea(), h2.getArea()));
+    public static List<String> buildApartmentsList(List<Home> homes, int n) {
+        // Проверяем входные данные
+        if (homes == null || homes.isEmpty() || n <= 0 || n > homes.size()) {
+            return Collections.emptyList(); // Возвращаем пустой список, если входные данные некорректны
+        }
+
+        // Сортируем список объектов по площади по возрастанию
+        Collections.sort(homes, (h1, h2) -> Double.compare(h1.getArea(), h2.getArea()));
+
+        // Берем первые n элементов
         List<Home> firstNHomes = homes.subList(0, n);
+
+        // Создаем список строковых представлений этих объектов
         List<String> result = new ArrayList<>();
         for (Home home : firstNHomes) {
             result.add(home.toString());
@@ -17,7 +26,9 @@ public class App {
 
         return result;
     }
+
 }
+
 
 
 // END
