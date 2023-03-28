6bf89dcb5f0739fb7208c7d5947eed65c2d709d3
@hexlet/cli <support@hexlet.io>
2023-03-29 01:34:46 +0500

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -1,32 +1,27 @@
 package exercise;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class App {
+class App {
 
-    public static List<String> buildApartmentsList(List<Home> homes, int n) {
-        // Проверяем входные данные
-        if (homes == null || homes.isEmpty() || n <= 0 || n > homes.size()) {
-            return Collections.emptyList(); // Возвращаем пустой список, если входные данные некорректны
-        }
-
-        // Сортируем список объектов по площади по возрастанию
-        Collections.sort(homes, (h1, h2) -> Double.compare(h1.getArea(), h2.getArea()));
-
-        // Берем первые n элементов
-        List<Home> firstNHomes = homes.subList(0, n);
-
-        // Создаем список строковых представлений этих объектов
-        List<String> result = new ArrayList<>();
-        for (Home home : firstNHomes) {
-            result.add(home.toString());
-        }
-
-        return result;
+    // new
+    public static List<String> buildApartmentsList(List<Home> apartments, int count) {
+        return apartments.stream()
+                .sorted(Home::compareTo)
+                .limit(count)
+                .map(Home::toString)
+                .toList();
     }
-
+    // old
+    public static List<String> buildApartmentsList2(List<Home> apartments, int count) {
+        int normalizedCount = Math.min(count, apartments.size());
+        apartments.sort(Home::compareTo);
+        List<Home> sublist = apartments.subList(0, normalizedCount);
+        return sublist.stream()
+                .map(appartment -> appartment.toString())
+                .collect(Collectors.toList());
+    }
 }
 
 
