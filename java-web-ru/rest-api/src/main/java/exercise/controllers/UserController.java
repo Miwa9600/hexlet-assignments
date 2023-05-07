0dcdad131a690c652d1f570af5ce7073761d2379
@hexlet/cli <support@hexlet.io>
2023-05-08 00:33:14 +0500

    submit java-web-ru/rest-api

--- java-web-ru/rest-api/src/main/java/exercise/controllers/UserController.java
+++ java-web-ru/rest-api/src/main/java/exercise/controllers/UserController.java
@@ -15,67 +15,58 @@ public class UserController implements CrudHandler {
 
     public void getAll(Context ctx) {
         // BEGIN
-        List<User> users = new QUser().findList();
+        List<User> users = new QUser()
+                .orderBy()
+                .id.asc()
+                .findList();
         String json = DB.json().toJson(users);
-        ctx.result(json);
+
+        ctx.json(json);
         // END
     };
 
     public void getOne(Context ctx, String id) {
 
         // BEGIN
-        User user = new QUser().setId(id).findOne();
-        if (user != null) {
-            String json = DB.json().toJson(user);
-            ctx.result(json);
-        } else {
-            ctx.status(404);
-        }
+        User user = new QUser()
+                .id.equalTo(Long.parseLong(id))
+                .findOne();
+
+        String json = DB.json().toJson(user);
+
+        ctx.json(json);
         // END
     };
 
     public void create(Context ctx) {
 
         // BEGIN
-        User user = DB.json().toBean(User.class, ctx.body());
-        if (user != null) {
-            user.save();
-            String json = DB.json().toJson(user);
-            ctx.result(json).status(201);
-        } else {
-            ctx.status(400);
-        }
+        User user = ctx.bodyValidator(User.class)
+                .check(it -> it.getFirstName().length() > 0, "First name can not be empty")
+                .check(it -> it.getLastName().length() > 0, "Last name can not be empty")
+                .check(it -> EmailValidator.getInstance().isValid(it.getEmail()), "Should be valid email")
+                .check(it -> StringUtils.isNumeric(it.getPassword()), "Password must contains only digits")
+                .check(it -> it.getPassword().length() >= 4, "Password must contain at least 4 characters")
+                .get();
+
+        user.save();
         // END
     };
 
     public void update(Context ctx, String id) {
         // BEGIN
-        User existingUser = new QUser().setId(id).findOne();
-        if (existingUser != null) {
-            User updatedUser = DB.json().toBean(User.class, ctx.body());
-            if (updatedUser != null) {
-                existingUser.setId(id);
-                existingUser.update();
-                String json = DB.json().toJson(existingUser);
-                ctx.result(json);
-            } else {
-                ctx.status(400);
-            }
-        } else {
-            ctx.status(404);
-        }
+        String body = ctx.body();
+        User updatedUser = DB.json().toBean(User.class, body);
+        updatedUser.setId(id);
+        updatedUser.update();
         // END
     };
 
     public void delete(Context ctx, String id) {
         // BEGIN
-        User user = DB.find(User.class, id);
-        if (user != null) {
-            user.delete();
-            ctx.status(204);
-        } else {
-            ctx.status(404);
-        }
+        new QUser()
+                .id.equalTo(Long.parseLong(id))
+                .delete();
         // END
     };
 }
